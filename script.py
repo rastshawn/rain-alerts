405b669a22868111060b4c801440114713870862
Shawn Rast <rastshawn@gmail.com>
2020-02-05 22:12:47 -0600

    custom messages now work properly

--- script.py
+++ script.py
@@ -5,23 +5,42 @@ import sys
 def getWeather():
     header = {"user-agent" : "Mozilla"}
     conn = http.client.HTTPSConnection("api.weather.gov")
-    conn.request("GET", "/gridpoints/HGX/86,75", headers=header)
-    httpResponse = conn.getresponse()
-    responseRAW = httpResponse.read()
-    conn.close()
-    weatherData = json.loads(responseRAW)
-    values = weatherData['properties']['quantitativePrecipitation']['values']
+    values = None
+    try:
+        conn.request("GET", "/gridpoints/HGX/86,75", headers=header)
+        httpResponse = conn.getresponse()
+        responseRAW = httpResponse.read()
+        conn.close()
+        weatherData = json.loads(responseRAW)
+        values = weatherData['properties']['quantitativePrecipitation']['values']
+    except:
+        conn.close()
+
     return values
 
 def makeCall(message):
     key = sys.argv[1]
-    url = 'https://maker.ifttt.com/trigger/heavy_rain_call/with/key/' + key + '?value1="Rain tomorrow"'
-    print(url) #test
+
+    body = {
+        'value1' : message
+    }
+    encodedBody = json.dumps(body).encode('utf-8')
+    header = {
+        "Content-Type" : "application/json; charset=utf-8",
+        "Content-Length" : len(encodedBody)
+    }
+    conn = http.client.HTTPSConnection("maker.ifttt.com")
+    conn.request("POST", '/trigger/heavy_rain_call/with/key/' + key,
+        encodedBody,
+        headers=header)
+    httpResponse = conn.getresponse()
+    print(httpResponse)
+    conn.close()
 
 if __name__ == "__main__":
-    makeCall(None)
-    weatherValues = getWeather()
-    print(weatherValues)
+    makeCall("I can make it say whatever I want to")
+    #weatherValues = getWeather()
+    #print(weatherValues)
 
 #     (response).properties.quantitativePrecipitation.values[]
 # Each of the above is an obj: {
